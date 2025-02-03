1b79958f41a4fb4ec675413b1f744130fd192d86
rickert156 <rickert156@rambler.ru>
2025-02-03 10:54:08 +0400

    add deploy script

--- auto.sh
+++ auto.sh
@@ -1,10 +1,11 @@
 cp -r SinCity ../Sin
 cp -r case ../case
 cp -r tmp ../tmp
-cp __init__ ../
+cp __init__.py ../
 cp requirements.txt ../requirements.txt
 cd ..
-rm -r SinCity
+rm -rf SinCity
+mv Sim SinCity
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
