8cd74deafcd3b08655431e0d587b576f2efaf5ea
rickert156 <rickert156@rambler.ru>
2025-02-04 12:18:36 +0400

    add module for getting list proxy

--- SinCity/Proxy/read_list_proxy.py
+++ SinCity/Proxy/read_list_proxy.py
@@ -1,4 +1,5 @@
 import json
+from manifest import proxy_json_path
 
 def add_proxy_to_list(proxy:str, list_proxy:str):
     list_proxy.append(proxy)
@@ -7,7 +8,7 @@ def add_proxy_to_list(proxy:str, list_proxy:str):
 
 def read_json():
     list_http, list_https = [], []
-    with open('proxy.json', 'r') as file:
+    with open(proxy_json_path, 'r') as file:
         result = json.load(file)
         for proxy_http in result['http']:
             get_list_http = add_proxy_to_list(proxy=proxy_http, list_proxy=list_http)
