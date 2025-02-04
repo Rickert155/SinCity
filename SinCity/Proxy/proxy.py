4bda85b4ac77f70a10f32aacaadc5b23f2bc1158
rickert156 <rickert156@rambler.ru>
2025-02-04 20:36:37 +0400

    mini worked for proxy utils

--- SinCity/Proxy/proxy.py
+++ SinCity/Proxy/proxy.py
@@ -1,6 +1,24 @@
-import requests, json, os
+import requests, json, os, time
 from SinCity.Proxy.check_dir import check_work_dir
 from SinCity.Proxy.read_list_proxy import read_json
+from bs4 import BeautifulSoup
+
+list_dict = []
+
+def collectet_proxy(processing_proxy:dict):
+    global list_dict
+
+    ip = processing_proxy.split(':')[0]
+    port = processing_proxy.split(':')[1]
+    data = {ip:port}
+    list_dict.append(data)
+
+def parser_proxy(proxy_url:str):
+    response = requests.get(proxy_url)
+    if response.status_code == 200:
+        for proxy in response.text.splitlines():
+            proxy = proxy.strip()
+            if proxy and ':' in proxy:collectet_proxy(proxy)
 
 
 def get_proxy():
@@ -9,5 +27,14 @@ def get_proxy():
     
     #получаем списки прокси
     list_http, list_https = read_json()
+    
+    list_proxie = [list_http, list_https]
 
-    print(f'{list_http}\n{list_https}')
+    for proxies in list_proxie:
+        for proxy in proxies:
+            print(proxy)
+            parser_proxy(proxy_url=proxy)
+            time.sleep(2)
+        
+    
+    print(len(list_dict))
