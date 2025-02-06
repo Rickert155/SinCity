bd9176d75b598dbad0993e57d6f9f83abe57917f
rickert156 <rickert156@rambler.ru>
2025-02-06 15:59:32 +0400

    delete module get proxy

--- manifest.py
+++
@@ -1,9 +0,0 @@
-sincity = 'SinCity'
-module_agent = 'Agent'
-module_web = 'Web'
-module_proxy = 'Proxy'
-
-case_proxy = 'case'
-case_proxy_path = f'{sincity}/{module_proxy}/{case_proxy}'
-proxy_json_path = f'{case_proxy_path}/proxy.json'
-proxy_json = f'{case_proxy_path}/all_proxy.json'
