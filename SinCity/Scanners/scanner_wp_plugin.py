a77150a59ccb6e79682026b0c9105178a60bcd31
rickert156 <rickert156@rambler.ru>
2025-02-23 16:18:09 +0400

    wp scanner plugin

--- SinCity/Scanners/scanner_wp_plugin.py
+++ SinCity/Scanners/scanner_wp_plugin.py
@@ -1,5 +1,7 @@
-import requests
+import requests, json, os, time
 from SinCity.Agent.header import header
+
+DOMAIN = None
 
 def get_domain():
     domain = input("Domain: ")
@@ -34,8 +36,28 @@ def plugins(file_name:str=None):
 
 counter_plugin = 0
 
+def recording(data:dict):
+    global DOMAIN
+    file_name = f'{DOMAIN}.json'
+    if not os.path.exists(file_name):
+        with open(file_name, 'w') as file:
+            info = {DOMAIN:[]}
+            json.dump(info, file, indent=4)
+
+    with open(file_name, 'r') as file:
+        info = json.load(file)
+    
+    data["date"] = time.strftime("%d/%m/%Y")
+    data["time"] = time.strftime("%H:%M")
+
+    info[DOMAIN].append(data)
+    with open(file_name, 'w') as file:
+        json.dump(info, file, indent=4)
+
+
 def processing_readme(response):
     global counter_plugin
+    global LIST_PLUGINS
     divide_line = '*'*40
     
     pattern_line_tag = 'Stable tag: '
@@ -62,6 +84,8 @@ def processing_readme(response):
             f'Version: \t{plugin_version}\n'
             f'{divide_line}\n'
             )
+    data = {"plugin_name":plugin_name, "plugin_version":plugin_version}
+    recording(data=data)
 
 def scan_url(address:str):
     global counter_plugin
@@ -69,29 +93,39 @@ def scan_url(address:str):
     try:
         response = requests.get(address, headers=head)
         if response.status_code != 404:
-            counter_plugin+=1
             address_readme = f'{address}/readme.txt'
             response = requests.get(address_readme, headers=head)
-            if response.status_code == 200:processing_readme(response)
+            if response.status_code == 200:
+                counter_plugin+=1
+                processing_readme(response)
     except requests.exceptions.ConnectionError as err:print(f'Error connect domain')
 
 def processing(domain:str):
     add_protocol = 'http://'
     add_url = '/wp-content/plugins/'
-
     url = f'{add_protocol}{domain}{add_url}'
-
-    list_plugins = plugins(file_name='plugins.txt')
-    if list_plugins != None:
-        for plugin in list_plugins:
-            full_address_plugin = f'{url}{plugin}'
-            scan_url(address=full_address_plugin)
     
-
+    url_admin = f'{add_protocol}{domain}/wp-login.php'
+    response = requests.get(url_admin)
+    if response.status_code != 404:
+        list_plugins = plugins(file_name='plugins.txt')
+        if list_plugins != None:
+            for plugin in list_plugins:
+                full_address_plugin = f'{url}{plugin}'
+                scan_url(address=full_address_plugin)
+    else:print(f'{domain} not WordPress...')
 
 def Scanner():
+    global DOMAIN
     try:
         domain = get_domain()
-        if domain != None:processing(domain=domain)
+        DOMAIN, domain_info_json = domain, f'{domain}.json'
+        if os.path.exists(domain_info_json):os.remove(domain_info_json)
+        if domain != None:
+            print(f'Processing domain {domain}...')
+            try:
+                processing(domain=domain)
+            except requests.exceptions.ConnectionError as err:print(err)
+
     except KeyboardInterrupt:print('\nExit...')
 
